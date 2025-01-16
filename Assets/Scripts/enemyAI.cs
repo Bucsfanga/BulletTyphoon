8667e0137d3a6dc9f3d8ab419f587c6f95386a0b
Roxythe (Rocky) Harding <roxythelharding@gmail.com>
2025-01-16 10:51:04 -0500

    Update enemyAI.cs

--- Assets/Scripts/enemyAI.cs
+++ Assets/Scripts/enemyAI.cs
@@ -1,135 +1,21 @@
 using UnityEngine;
-using UnityEngine.AI;
-using System.Collections;
 
-public class EnemyAI : MonoBehaviour, IDamage
+public class EnemyController : MonoBehaviour
 {
-    [SerializeField] NavMeshAgent agent;
-    [SerializeField] Animator anim;
-    [SerializeField] Renderer model;
-    [SerializeField] Transform shootPos, headPos;
-
-    [SerializeField] int HP;
-    [SerializeField] int faceTargetSpeed;
-    [SerializeField] int FOV;
-    [SerializeField] int animSpeedTrans;
-
-    [SerializeField] GameObject bullet;
-    [SerializeField] float shootRate;
-    [SerializeField] int shootrange;
-
-    float angleToPlayer;
-
-    bool isShooting;
-    bool playerInRange;
-
-    Color colorOrig;
-
-    Vector3 playerDir;
-
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        colorOrig = model.material.color;
-        GameManager.instance.updateGameGoal(1);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        float agentSpeed = agent.velocity.normalized.magnitude;
-        float animSpeed = anim.GetFloat("speed");
-
-        anim.SetFloat("Speed", Mathf.MoveTowards(animSpeed, agentSpeed, Time.deltaTime * animSpeedTrans));
-
-        if (playerInRange && canSeePlayer())
-        {
-            if (!isShooting)
-            {
-                StartCoroutine(shoot());
-            }
-        }
+        
     }
 
-    private void OnTriggerEnter(Collider other)
+    public void takeDamnage(int amount)
     {
-        if(other.CompareTag("Playyer"))
-        {
-            playerInRange = true;
-        }
-    } 
-    private void OnTriggerExit(Collider other)
-    {
-        if(other.CompareTag("Playyer"))
-        {
-            playerInRange = false;
-        }
-    }
 
-    bool canSeePlayer()
-    {
-        playerDir = GameManager.instance.player.transform.position - transform.position;
-        angleToPlayer = Vector3.Angle(playerDir, transform.forward);
-
-        Debug.DrawRay(transform.position, playerDir);
-
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, playerDir, out hit))
-        {
-            // Hey can you see the player
-            if(hit.collider.CompareTag("Player") && angleToPlayer <= FOV)
-            {
-                agent.SetDestination(GameManager.instance.player.transform.position);
-
-                if (agent.remainingDistance <= agent.stoppingDistance)
-                {
-                    faceTarget();
-                }
-
-                if (!isShooting)
-                {
-                    StartCoroutine(shoot());
-                }
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    void faceTarget()
-    {
-        Quaternion rot = Quaternion.LookRotation(new Vector3(playerDir.x, 0, playerDir.z));
-        transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * faceTargetSpeed);
-    }
-
-    public void takeDamage(int amount)
-    {
-        HP -= amount;
-
-        StartCoroutine(flashRed());
-
-        if (HP <= 0)
-        {
-            GameManager.instance.updateGameGoal(-1);
-            Destroy(gameObject);
-        }
-    }
-
-    IEnumerator flashRed()
-    {
-        model.material.color = Color.red;
-        yield return new WaitForSeconds(0.1f);
-        model.material.color = colorOrig;
-    }
-
-    IEnumerator shoot()
-    {
-        isShooting = true;
-        Instantiate(bullet, shootPos.position, transform.rotation);
-
-        yield return new WaitForSeconds(shootRate);
-        isShooting = false;
     }
 }
