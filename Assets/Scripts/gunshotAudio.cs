eeaa986bddb6261c53b0d2820802fc1db9dd31d0
Roxythe (Rocky) Harding <roxythelharding@gmail.com>
2025-01-27 01:23:03 -0500

    Merge branch 'UnitTest' into Rocky

--- Assets/Scripts/gunshotAudio.cs
+++ Assets/Scripts/gunshotAudio.cs
@@ -2,61 +2,32 @@ using UnityEngine;
 
 public class gunshotAudio : MonoBehaviour
 {
-    [Header("Audio Sources")]
-    [SerializeField] private AudioSource gunAudioSource;
-
-    [Header("Audio Clips")]
-    [SerializeField] private AudioClip gunShotClip;
-    [SerializeField] private AudioClip gunClickClip;
-    [SerializeField] private AudioClip gunReloadClip;
-
-    [Header("Input Detection")]
-    [SerializeField] private string fireButton = "Shoot";
-    [SerializeField] private string reloadButton = "Reload";
-    [SerializeField] private bool useInputDetection = true;
-
-    [Header("Audio Settings")]
+    private audioManager audioManager;
+  
     [SerializeField] private float volumeMultiplier = 1f;
-    [SerializeField] private bool useRandomPitch = true;
+    [SerializeField] public bool useRandomPitch = true;
     [SerializeField] private float minPitch = 0.95f;
     [SerializeField] private float maxPitch = 1.05f;
 
-    
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Awake()
+    void Start()
     {
-        // If the audio source is not set, add an audio source to the game object
-        if (gunAudioSource == null)
-        {
-            gunAudioSource = gameObject.AddComponent<AudioSource>();
-            gunAudioSource.playOnAwake = false;
-        }
+        audioManager = audioManager.instance;        
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown(fireButton))
-        {
-            PlayGunShot();
-        }
     }
 
-    void PlayGunShot()
+    public void PlayGunShot()
     {
+        //TODO:: Incorporate a random pitch method/setting in audio manager
         if (useRandomPitch)
         {
-            gunAudioSource.pitch = Random.Range(minPitch, maxPitch);
+            audioManager.PlaySoundWithPitch("Gun Shot", minPitch, maxPitch);
         }
-        gunAudioSource.PlayOneShot(gunShotClip, volumeMultiplier);
-    }
-
-    void PlayGunReload()
-    { 
-    if(Input.GetButtonDown(reloadButton))
+        else
         {
-            gunAudioSource.PlayOneShot(gunReloadClip);
+            audioManager.PlaySound("Gun Shot");
         }
     }
 }
