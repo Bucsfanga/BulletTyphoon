92de58785d18b67d93fccf3aa42abedc43eaabd0
Roxythe (Rocky) Harding <roxythelharding@gmail.com>
2025-02-26 22:12:24 -0500

    Revert "Fixed merge conflicts"
    This reverts commit dc027072cf12885756bd404cd80a6e90ca3e4fae, reversing
    changes made to cbdae927939e3a6f13b340b434f9bce6d061a850.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -28,15 +28,12 @@ public class GameManager : MonoBehaviour
     [Header("-----UI Menus-----")]
     [SerializeField] GameObject menuPause;
     [SerializeField] GameObject menuActive;
-    [SerializeField] GameObject menuClassifiedDoc;
-    [SerializeField] public GameObject menuWin;
+    [SerializeField] GameObject menuWin;
     [SerializeField] GameObject menuLose;
     [SerializeField] GameObject menuMain;
     [SerializeField] GameObject menuSettings;
     [SerializeField] GameObject menuCredits;
     [SerializeField] GameObject menuControls;
-    [SerializeField] GameObject menuTimers;
-    [SerializeField] public GameObject menuTally;
     private GameObject lastMenu;
 
     public float levelStartTime;
@@ -129,7 +126,6 @@ public class GameManager : MonoBehaviour
             playerHUD.SetActive(true);
             menuPause.SetActive(false);
             menuMain.SetActive(false);
-            menuTimers.SetActive(true);
             Time.timeScale = 1f;
             isPaused = false;
             Cursor.visible = false;
@@ -272,7 +268,6 @@ public class GameManager : MonoBehaviour
         menuActive = menuMain;
         playerHUD.SetActive(false);
         menuPause.SetActive(false);
-        menuTimers.SetActive(false);
         statePause();
 
         if (player != null)
@@ -306,7 +301,6 @@ public class GameManager : MonoBehaviour
         //StartCoroutine(ContrtolsScreen());
         playerHUD.SetActive(true);  // Show playerHUD
         menuPause.SetActive(false);
-        menuTimers.SetActive(true);
         Time.timeScale = 1f;  // Resume the game
         isPaused = false;
         audioManager.instance.StopMenuMusic(); //Ian add - fade out the menu music as the game starts
@@ -384,11 +378,9 @@ public class GameManager : MonoBehaviour
     // Track win condition of player reaching goal position on level
     public void updateGameWinCondition(int amount)
     {
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        goalCheckpoint = currentSceneIndex;
-        goalCheckpoint = amount;
+        goalCheckpoint += amount;
 
-        if (goalCheckpoint >= 0)
+        if (goalCheckpoint >= 1)
         {
             if (SceneManager.GetActiveScene().name != "UnitTestLevel4")
             {
@@ -408,7 +400,6 @@ public class GameManager : MonoBehaviour
         {
             menuMain.SetActive(false);
             menuCredits.SetActive(true);
-            menuTimers.SetActive(false);
             menuActive = menuCredits;
 
             // Trigger credit scroller
@@ -662,7 +653,6 @@ public class GameManager : MonoBehaviour
 
         // Open the Settings Menu
         menuSettings.SetActive(true);
-        menuTimers.SetActive(false);
         menuActive = menuSettings;
         SelectFirstButton(menuSettings);
     }
@@ -692,7 +682,6 @@ public class GameManager : MonoBehaviour
             if (lastMenu == menuPause)
             {
                 playerHUD.SetActive(true);
-                menuTimers.SetActive(true);
             }
             SelectFirstButton(lastMenu);
         }
@@ -704,7 +693,6 @@ public class GameManager : MonoBehaviour
         // Hide Pause Menu & HUD
         menuPause.SetActive(false);
         playerHUD.SetActive(false);
-        menuTimers.SetActive(false);
 
         // Show Main Menu UI
         menuMain.SetActive(true);
@@ -829,7 +817,6 @@ public class GameManager : MonoBehaviour
     public void OpenControlMenu()
     {
         menuPause.SetActive(false);
-        menuTimers.SetActive(false);
         menuControls.SetActive(true);
         menuActive = menuControls;
     }
@@ -838,7 +825,6 @@ public class GameManager : MonoBehaviour
     public void CloseControlMenu()
     {
         menuControls.SetActive(false);
-        menuTimers.SetActive(true);
         menuPause.SetActive(true);
         menuActive = menuPause;
     }
@@ -899,7 +885,9 @@ public class GameManager : MonoBehaviour
 
 
     public void EndLevel()
-    {        
+    {
+        
+
         float completionTime = Time.timeSinceLevelLoad;
         int damageTaken = 0;
         int stepsTaken = 0;
@@ -944,16 +932,8 @@ public class GameManager : MonoBehaviour
         }
 
         SaveHighScores(highScores);
+
        
-        float bestTime = PlayerPrefs.GetFloat("HighScore", float.MaxValue);
-
-        // If the new time is better, save it
-        if (newScore < bestTime)
-        {
-            PlayerPrefs.SetFloat("HighScore", newScore);
-            PlayerPrefs.Save();
-        }
-
     }
     public List<float> LoadHighScores()
     {
@@ -981,7 +961,6 @@ public class GameManager : MonoBehaviour
     {
         if (tutorialPanel != null)
         {
-            menuTimers.SetActive(false);
             tutorialPanel.SetActive(true); // Turn on tutorial screen
             statePause(); // Pause game
 
@@ -1006,7 +985,6 @@ public class GameManager : MonoBehaviour
     {
         if (tutorialPanel != null)
         {
-            menuTimers.SetActive(true);
             tutorialPanel.SetActive(false); // Hide tutorial panel
             stateUnpause(); // Unpause game
         }
